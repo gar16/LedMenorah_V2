b73c5e7f19f4ff4c1a1f45d969c3c413b13cbeed
gar16 <garber16@gmail.com>
2018-09-28 17:33:29 -0400

    Update ledmenorahinc.h

--- ledmenorahinc.h
+++ ledmenorahinc.h
@@ -1,8 +1,12 @@
 /*
  * menorahinc.h
  *
+ * Created: 10/18/2015 3:31:51 AM
+ * V2 9/2018
  *  Author: Pin
  */ 
+
+
 #ifndef MENORAHINC_H_
 #define MENORAHINC_H_
 
@@ -11,21 +15,19 @@
 #define pportB 0x018
 #define pportD 0x012
 //define LEDs for nights
-#define eightN		PORTD6
-#define seventhN	PORTB0
-#define sixthN		PORTD5
-#define fifthN		PORTD4
-#define fourthN		PORTD3
-#define thirdN		PORTD2
-#define secondN		PORTA0
-#define firstN		PORTA1
+#define eightN		PORTD6 //on v1 PORTA0
+#define seventhN	PORTB0 //on v1 PORTD2
+#define sixthN		PORTD5 //on v1 PORTD3
+#define fifthN		PORTD4 //on v1 PORTD4
+#define fourthN		PORTD3 //on v1 PORTD5
+#define thirdN		PORTD2 //on v1 PORTD6
+#define secondN		PORTA0 //on v1 PORTB0
+#define firstN		PORTA1 //on v1 PORTB1
 #define button		PORTB3
-
 //progmem to put constant into flash to save stack space
-const uint8_t ledPins []  PROGMEM =  {0, firstN, secondN, thirdN, fourthN, fifthN, sixthN, seventhN, eightN};
-const uint8_t ledPorts []  PROGMEM = {0, pportA, pportA,  pportD, pportD,  pportD, pportD, pportB,   pportD};
-
-//define functions
+const uint8_t ledPins []  PROGMEM =  {0, firstN, secondN, thirdN, fourthN, fifthN, sixthN, seventhN, eightN};//pins for each LED from V1
+const uint8_t ledPorts []  PROGMEM = {0, pportA, pportA,  pportD, pportD,  pportD, pportD, pportB,   pportD} ;//ports matching LEDs from V1
+//define functionsr
 void turn_on_lights(int);
 void setup_timer0_PWM();
 void setup_timer1();
