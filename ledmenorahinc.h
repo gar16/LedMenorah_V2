f87aaed8b1f444bfb382cb94044cc72fa8abcb1b
gar16 <garber16@gmail.com>
2018-09-28 16:07:40 -0400

    Update ledmenorahinc.h

--- ledmenorahinc.h
+++ ledmenorahinc.h
@@ -1,7 +1,9 @@
 /*
  * menorahinc.h
- * Author: Pin
+ *
+ *  Author: Pin
  */ 
+
 
 #ifndef MENORAHINC_H_
 #define MENORAHINC_H_
@@ -10,23 +12,20 @@
 #define pportA 0x01B
 #define pportB 0x018
 #define pportD 0x012
-
 //define LEDs for nights
-#define eightN		PORTA0
-#define seventhN	PORTD2
-#define sixthN		PORTD3
+#define eightN		PORTD6
+#define seventhN	PORTB0
+#define sixthN		PORTD5
 #define fifthN		PORTD4
-#define fourthN		PORTD5
-#define thirdN		PORTD6
-#define secondN		PORTB0
-#define firstN		PORTB1
+#define fourthN		PORTD3
+#define thirdN		PORTD2
+#define secondN		PORTA0
+#define firstN		PORTA1
 #define button		PORTB3
-
 //progmem to put constant into flash to save stack space
-const uint8_t ledPins []  PROGMEM =  {0, firstN, secondN, thirdN, fourthN, fifthN, sixthN, seventhN, eightN};//pins for each LED
-const uint8_t ledPorts []  PROGMEM = {0, pportB, pportB,  pportD, pportD,  pportD, pportD, pportD,   pportA} ;//ports matching LEDs
-
-//functions
+const uint8_t ledPins []  PROGMEM =  {0, firstN, secondN, thirdN, fourthN, fifthN, sixthN, seventhN, eightN};
+const uint8_t ledPorts []  PROGMEM = {0, pportA, pportA,  pportD, pportD,  pportD, pportD, pportB,   pportD};
+//define functionsr
 void turn_on_lights(int);
 void setup_timer0_PWM();
 void setup_timer1();
@@ -37,5 +36,4 @@ void led_in_out();
 void go_to_sleep();
 void night_select();
 void check_button();
-
 #endif /* MENORAHINC_H_ */
